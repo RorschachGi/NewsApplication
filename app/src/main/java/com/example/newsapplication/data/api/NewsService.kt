b28bf0a0ce468cfd1f13cb890847c6615559e521
RorschachGi <tumanovAccount7773@rambler.ru>
2023-10-07 12:45:04 +0400

    work with Room, create ViewModel

--- app/src/main/java/com/example/newsapplication/data/api/NewsService.kt
+++ app/src/main/java/com/example/newsapplication/data/api/NewsService.kt
@@ -1,6 +1,8 @@
 package com.example.newsapplication.data.api
 
+import com.example.newsapplication.models.NewsResponse
 import com.example.newsapplication.utils.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,12 +13,13 @@ interface NewsService {
         @Query("q") query: String,
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 
+    @GET("/v2/top-headlines")
     suspend fun getHeadLines(
         @Query("country") countryCode: String = "ru",
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 
 }
