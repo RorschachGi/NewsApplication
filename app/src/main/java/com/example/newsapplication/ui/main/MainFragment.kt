48017b70796187e26be8666db1b1f3a93cf00832
RorschachGi <tumanovAccount7773@rambler.ru>
2023-10-10 13:38:21 +0400

    create RecyclerView and search fragment

--- app/src/main/java/com/example/newsapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapplication/ui/main/MainFragment.kt
@@ -1,15 +1,16 @@
 package com.example.newsapplication.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.newsapplication.R
-import com.example.newsapplication.databinding.ActivityMainBinding
-import com.example.newsapplication.databinding.FragmentDetailsBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapplication.databinding.FragmentMainBinding
+import com.example.newsapplication.ui.adapters.NewsAdapter
+import com.example.newsapplication.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +20,7 @@ class MainFragment : Fragment() {
     private val mBinding get() = _binding!!
 
     private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +32,34 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner){response ->
+            when(response){
+                is Resource.Error -> {
+                    mBinding.pagProgressBar.visibility = View.INVISIBLE
+                    response.data?.let{
+                        Log.e("MainFragment", "MainFragment error ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    mBinding.pagProgressBar.visibility = View.VISIBLE
+                }
+                is Resource.Success -> {
+                    mBinding.pagProgressBar.visibility = View.INVISIBLE
+                    response.data?.let{
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        mBinding.newsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
